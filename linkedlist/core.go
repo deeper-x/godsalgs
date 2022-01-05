a3818664c7ec8b959c04947ec1953a13c59fc1f8
deeper-x <albertodeprezzo@gmail.com>
2022-01-05 16:37:28 +0100

    basic refactoring and unittest

--- linkedlist/core.go
+++ linkedlist/core.go
@@ -1,9 +1,24 @@
 package linkedlist
 
+import (
+	"fmt"
+	"strconv"
+)
+
+var ll string
+var c int
+
 // Node is the linked list node implementation
 type Node struct {
 	Value int
 	Next  *Node
+}
+
+// NewNode is the constructor for the linked list
+// It resets the linked list string as well
+func NewNode(v int) *Node {
+	ll = ""
+	return &Node{Value: v}
 }
 
 // Add adds a new node to the linked list
@@ -12,6 +27,7 @@ func (n *Node) Add(v int) bool {
 		n.Next = &Node{Value: v}
 		return true
 	}
+
 	return n.Next.Add(v)
 }
 
@@ -24,15 +40,13 @@ func (n *Node) Delete(v int) bool {
 		n.Next = n.Next.Next
 		return true
 	}
+
 	return n.Next.Delete(v)
 }
 
 // Head returns the head of the linked list
 func (n *Node) Head() *Node {
-	if n.Next == nil {
-		return n
-	}
-	return n.Next.Head()
+	return n
 }
 
 // Tail returns the tail of the linked list
@@ -40,14 +54,47 @@ func (n *Node) Tail() *Node {
 	if n.Next == nil {
 		return n
 	}
+
 	return n.Next.Tail()
 }
 
 // Replace replaces a node with another node
 func (n *Node) Replace(v, w int) bool {
+	c++
+	tot := n.Length()
+
+	if c > tot {
+		return false
+	}
+
 	if n.Value == v {
 		n.Value = w
 		return true
 	}
+
 	return n.Next.Replace(v, w)
+}
+
+// Length returns the length of the linked list
+func (n *Node) Length() int {
+	if n.Next == nil {
+		return 1
+	}
+
+	return 1 + n.Next.Length()
+}
+
+// Print prints the linked list
+func (n *Node) Print() string {
+	if len(ll) == 0 {
+		ll += fmt.Sprintf("%v", strconv.Itoa(n.Value))
+	} else {
+		ll += fmt.Sprintf("->%v", strconv.Itoa(n.Value))
+	}
+
+	if n.Next != nil {
+		n.Next.Print()
+	}
+
+	return ll
 }
--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -1,7 +1,102 @@
 package linkedlist
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestLinkedList(t *testing.T) {
+func TestAdd(t *testing.T) {
+	n := NewNode(1)
+
+	ok := n.Add(2)
+	if !ok {
+		t.Error("Expected true, got false")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	n := NewNode(1)
+
+	n.Add(2)
+	n.Add(3)
+
+	ok := n.Delete(2)
+	if !ok {
+		t.Error("Expected true, got false")
+	}
+}
+
+func TestHead(t *testing.T) {
+	n := NewNode(1)
+
+	n.Add(2)
+	n.Add(3)
+
+	res := n.Head()
+
+	if res.Value != 1 {
+		t.Error("Expected 1, got " + strconv.Itoa(res.Value))
+	}
+}
+
+func TestTail(t *testing.T) {
+	n := NewNode(1)
+
+	n.Add(2)
+	n.Add(3)
+
+	res := n.Tail()
+	if res.Value != 3 {
+		t.Error("Expected 3, got " + strconv.Itoa(res.Value))
+	}
+}
+
+func TestPrint(t *testing.T) {
+	n := NewNode(1)
+	n.Add(2)
+	n.Add(3)
+
+	res := n.Print()
+	extd := "1->2->3"
+
+	if res != extd {
+		t.Error("Expected: " + extd + " Got: " + res)
+	}
+
+}
+
+func TestReplace(t *testing.T) {
+	n := NewNode(1)
+	n.Add(2)
+	n.Add(3)
+
+	ok := n.Replace(2, 4)
+	if !ok {
+		t.Error("replace should be true")
+	}
+
+}
+
+func TestLength(t *testing.T) {
+	n := NewNode(1)
+	n.Add(2)
+	n.Add(3)
+
+	res := n.Length()
+	if res != 3 {
+		t.Error("Expected 3, got " + strconv.Itoa(res))
+	}
+}
+
+func TestReplaceFail(t *testing.T) {
+	n := NewNode(1)
+	n.Add(2)
+	n.Add(3)
+
+	ok := n.Replace(5, 4)
+
+	if ok {
+		t.Error("replace should be false")
+	}
 
 }
